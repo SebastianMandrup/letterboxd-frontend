ddd5553ca98b21e4efc260f0267b8f6740908004
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2025-09-28 01:56:35 +0200

    i hate this

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import Index from '.index'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Index from './indexTemp'
 import './main.css'
 
 createRoot(document.getElementById('root')!).render(
