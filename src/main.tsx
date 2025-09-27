559a517aecd34aff7eb4e9b4cfa2f8b5eed8d0ff
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2025-09-28 01:54:36 +0200

    ja

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Index from './index'
+import Index from './indexTemp'
 import './main.css'
 
 createRoot(document.getElementById('root')!).render(
